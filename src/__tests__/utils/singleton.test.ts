387c5152322af387b53e8b2babb665159264e2c4
evuz <jesus_jgb_45@hotmail.com>
2019-01-05 09:06:28 +0100

    feat: create singleton like a function to keep the original context

--- src/__tests__/utils/singleton.test.ts
+++ src/__tests__/utils/singleton.test.ts
@@ -1,4 +1,4 @@
-import { generateSingleton, Singleton } from '../../utils/singleton';
+import { generateSingleton, singleton } from '../../utils/singleton';
 
 // tslint:disable-next-line:max-classes-per-file
 class Foo {
@@ -43,34 +43,34 @@ describe('Utils => singleton', () => {
   });
 
   test('should instance a class', () => {
-    const baz = new Singleton(Baz, { param: 's' }).getInstance();
+    const baz = singleton(Baz, { param: 's' }).getInstance();
     expect(baz instanceof Baz).toBeTruthy();
   });
 
   test('should instance a class with params', () => {
     const param = 'test param';
-    const bar = new Singleton(Bar, param).getInstance();
+    const bar = singleton(Bar, param).getInstance();
     expect(bar.param).toBe(param);
   });
 
   test('should instance a class with several params', () => {
     const params = { x: 'param_x', y: 3, z: 'param_z' };
     const props = Object.keys(params).map(key => params[key]);
-    const bar = new Singleton(Foo, ...props).getInstance();
+    const bar = singleton(Foo, ...props).getInstance();
     Object.keys(params).forEach(key => {
       expect(bar[key]).toBe(params[key]);
     });
   });
 
   test('should instance class only one time', () => {
-    const qux = new Singleton(Qux);
+    const qux = singleton(Qux);
     const instance = qux.getInstance();
     expect(qux.getInstance()).toBe(instance);
   });
 
   test('should instance two differents class', () => {
-    const foo = new Singleton(Foo, 'one', 2, 'three').getInstance();
-    const bar = new Singleton(Bar, 'param').getInstance();
+    const foo = singleton(Foo, 'one', 2, 'three').getInstance();
+    const bar = singleton(Bar, 'param').getInstance();
     expect(foo).not.toBe(bar);
   });
 });
