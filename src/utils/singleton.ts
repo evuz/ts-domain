79b3b7f1d94ae07803b74affb09e4ed76011b3e9
evuz <jesus_jgb_45@hotmail.com>
2019-01-07 19:46:42 +0100

    Revert "feat: create singleton like a function to keep the original context"
    This reverts commit 387c5152322af387b53e8b2babb665159264e2c4.

--- src/utils/singleton.ts
+++ src/utils/singleton.ts
@@ -1,15 +1,13 @@
 import { IConstructor } from './types';
 export { generateSingleton } from './Singleton.fallback';
 
-export function singleton<T, U extends any[]>(singleton: IConstructor<T, U>, ...params: U) {
-  let instance: T;
-  instance = new singleton(...params);
-
-  function getInstance(): T {
-    return instance;
+export class Singleton<T, U extends any[]> {
+  private instance: T;
+  constructor(singleton: IConstructor<T, U>, ...params: U) {
+    this.instance = new singleton(...params);
   }
 
-  return {
-    getInstance,
-  };
+  public getInstance(): T {
+    return this.instance;
+  }
 }
